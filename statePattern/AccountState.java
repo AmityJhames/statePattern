6288cdd9ea8bcb3fb49db2d306e07988cc5578d8
Amity Jhames Abaja <143429028+AmityJhames@users.noreply.github.com>
2025-02-10 07:57:34 +0800

    Delete statePattern directory

--- statePattern/AccountState.java
+++
@@ -1,9 +0,0 @@
-package statePattern;
-
-public interface AccountState {
-    public void deposit(Double depositAmount, Account account);
-    public void withdraw(Double withdrawAmount, Account account);
-    public void activate(Account account);
-    public void close(Account account);
-    public void suspend(Account account);
-}
